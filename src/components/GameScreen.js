32b5f2843204598b5e3ee56fd703e46a529fd18a
MingMing <greeteaz567@gmail.com>
2022-08-20 14:44:21 -0500

    Refactored code

--- src/components/GameScreen.js
+++ src/components/GameScreen.js
@@ -5,22 +5,26 @@ import { useState } from "react";
 import CyberPunkSrc from "../assets/egor-klyuchnyk-full-x-season-web.jpg";
 import AshKetchum from "../assets/ash-ketch-1.png";
 import Tom from "../assets/tom-tom-and-jerry-png-418409.png"
+import { Navigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { MapInfo } from "./Menu";
 
-export const images = {
-    "CyberPunk": {src: CyberPunkSrc, characters: [{name: "Ash", image: AshKetchum}, {name: "Tom", image: Tom}, {name:"Patrick"}]},
-}
 
 export default function GameScreen({gameImage}) {
     const [displayCursor, setDisplayCursor] = useState(false);
     
+    const params = useParams();
+
     function cursorDisplay (e) {
         setDisplayCursor((prevState) => !prevState);
     }
 
+    const thisMap = MapInfo.find((map)=>Object.keys(map)[0] === params.mapName);
+
     return (
-        <React.Fragment>
-            <Header characters={images[gameImage].characters}/>
-            <Image gameImage={images[gameImage].src} gameCharacter={images[gameImage].characters} cursorState={cursorDisplay} displayCursor={displayCursor}/>
-        </React.Fragment>
-    )
+        <>
+    <Header characters={thisMap[params.mapName].characters}/>
+    <Image gameImage={thisMap[params.mapName].src} gameCharacter={thisMap[params.mapName].characters} cursorState={cursorDisplay} displayCursor={displayCursor}/>
+</> 
+    );
 }
