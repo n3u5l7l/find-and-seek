bce210682575eaa1f793f4ef021c053d3b43bffe
MingMing <greeteaz567@gmail.com>
2022-08-20 14:37:14 -0500

    Added page

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -1,10 +1,13 @@
-
+import React from "react";
+import styled from "styled-components";
+import MenuPage from "./Menu";
 
 export default function Leaderboard(){
 
     return(
-        <div>
-
-        </div>
+        <React.Fragment>
+            <h1>LeaderBoard</h1>
+            <MenuPage name="Leaderboard"/>
+        </React.Fragment>
     )
 }
---
+++ src/components/Map.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import Menu from "./Menu";
+
+const CustomHeader = styled.h1`
+    margin:0;
+    padding:0;
+`
+export default function Maps(){
+
+    return(
+        <>
+            <h1>Maps</h1>
+            <Menu name="Maps"/>
+        </>
+    )
+
+}
