218116df8e724f7aca9773ded21a1c6022be6ea9
MingMing <greeteaz567@gmail.com>
2022-08-20 14:45:17 -0500

    Refactored code

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -6,9 +6,9 @@ import { useCallback, useState } from "react";
 import particlesOptions from "../particles.json";
 import { motion } from "framer-motion";
 import { GameImageContext } from "../App";
-import MapMenu from "./MapMenu";
 import Leaderboard from "./LeaderBoard";
-
+import ApplyParticle from "./ApplyParticle";
+import { Link } from "react-router-dom";
 const StartPage = styled(motion.main)`
     display: flex;
     flex-direction: column;
@@ -45,49 +45,58 @@ const ButtonWrapper = styled.div`
     gap:10px;
     flex-direction: column;
 `;
+const MotionLink = motion(Link);
 
-export const CustomButton = styled(motion.button)`
+export const CustomButton = styled(MotionLink)`
     border: 1px solid white;
+    display:flex;
+    justify-content: center;
+    align-items:center;
     background-color: black;
     color:white;
     cursor: pointer;
+    text-decoration: none;
     opacity:0.7;
 `;
 
-function HomeMenu ({setStartScreen, setOptionScreen}){
+function HomeMenu ({setStartScreen, setOptionScreen, setLeaderboardScreen}){
 
     return (
-        <Menu>
+        <React.Fragment>
+            <h1>Find-And-Seek</h1>
+            <Menu>
                 <ButtonWrapper className="option-wrapper">
-                    <CustomButton whileHover={{scale:1.05, opacity:1}} whileTap={{scale:0.95, opacity:1}}  onClick={()=> {setOptionScreen((prevState)=>!prevState)}}><h3>Start Game</h3></CustomButton>
-                    <CustomButton whileHover={{scale:1.05, opacity:1}} whileTap={{scale:0.95, opacity:1}} ><h3>View LeaderBoard</h3></CustomButton>
+                    <CustomButton to="/menu" whileHover={{scale:1.05, opacity:1}} whileTap={{scale:0.95, opacity:1}}><h3>Start Game</h3></CustomButton>
+                    <CustomButton to="/leaderboard" whileHover={{scale:1.05, opacity:1}} whileTap={{scale:0.95, opacity:1}}><h3>View LeaderBoard</h3></CustomButton>
                 </ButtonWrapper>
-        </Menu>
+            </Menu>
+        </React.Fragment>
     )
 }
 
 
-export default function StartScreen({setStartScreen, setGameImage}){
-    const [optionScreen, setOptionScreen] = useState(false);
-    const [leaderboardScreen, setLeaderboardScreen] = useState(false);
-
-    const particlesInit = useCallback( async (engine) => {
-        await loadFull(engine);
-    }, []);
-
-    let content = (optionScreen && <MapMenu setGameImage={setGameImage} />) || (leaderboardScreen && <Leaderboard />) || <HomeMenu setOptionScreen={setOptionScreen}/>;
+export default function StartScreen({setStartScreen, setGameImage, setOptionScreen, setLeaderboardScreen}){
+    let screen = ApplyParticle({setStartScreen,  setOptionScreen, setLeaderboardScreen})(HomeMenu);
     return (
-        <StartPage  exit={{opacity:0}} transition={{duration:0.5}}  >
-            <Particle id="tsparticles" options={particlesOptions} init={particlesInit} />
-            <h1>Find-And-Seek</h1>
-            {content}
-        </StartPage>
+        screen
     )
 }
 
 
 
 /*
+    //const [optionScreen, setOptionScreen] = useState(false);
+    //const [leaderboardScreen, setLeaderboardScreen] = useState(false);
+   //onClick={()=> {setOptionScreen((prevState)=>!prevState); setStartScreen((prev)=>!prev)}
+   //onClick={() => {setLeaderboardScreen((prev)=>!prev)(}}
+  const particlesInit = useCallback( async (engine) => {
+        await loadFull(engine);
+    }, []); 
 
-
+    //let content = (optionScreen && <MapMenu setGameImage={setGameImage} />) || (leaderboardScreen && <Leaderboard />) || <HomeMenu setStartScreen={setStartScreen} setLeaderboardScreen={setLeaderboardScreen} setOptionScreen={setOptionScreen}/>;
+    //let screen = ApplyParticle(HomeMenu, {setStartScreen, setOptionScreen, setLeaderboardScreen});
+         <StartPage  exit={{opacity:0}} transition={{duration:0.5}}  >
+            <Particle id="tsparticles" options={particlesOptions} init={particlesInit} />
+            {content}
+        </StartPage> 
 */
