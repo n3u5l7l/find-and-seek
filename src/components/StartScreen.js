346641bc942aa96de886bb4b58ca728fe634c221
MingMing <greeteaz567@gmail.com>
2022-08-21 23:22:27 -0500

    Removed un-needed props and variables

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,37 +1,8 @@
-import React, { useContext } from "react";
+import React from "react";
 import styled from "styled-components";
-import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
-import { useCallback, useState } from "react";
-import particlesOptions from "../particles.json";
 import { motion } from "framer-motion";
-import { GameImageContext } from "../App";
-import Leaderboard from "./LeaderBoard";
 import ApplyParticle from "./ApplyParticle";
 import { Link } from "react-router-dom";
-const StartPage = styled(motion.main)`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap:25px;
-    min-width: 100vw;
-    min-height: 100vh;
-    width:100%;
-    margin:0; padding:0;
-    color:white;
-`;
-
-const Particle = styled(Particles)`
-    position: fixed;
-    height: 100%;
-    width: 100%;
-    margin: 0;
-    padding: 0;
-    left: 0;
-    top: 0;
-    z-index: -1;
-`;
 
 const Menu = styled.nav`
     display: flex;
@@ -59,7 +30,7 @@ export const CustomButton = styled(MotionLink)`
     opacity:0.7;
 `;
 
-function HomeMenu ({setStartScreen, setOptionScreen, setLeaderboardScreen}){
+function HomeMenu (){
 
     return (
         <React.Fragment>
@@ -75,28 +46,9 @@ function HomeMenu ({setStartScreen, setOptionScreen, setLeaderboardScreen}){
 }
 
 
-export default function StartScreen({setStartScreen, setGameImage, setOptionScreen, setLeaderboardScreen}){
-    let screen = ApplyParticle({setStartScreen,  setOptionScreen, setLeaderboardScreen})(HomeMenu);
+export default function StartScreen(){
+    let screen = ApplyParticle()(HomeMenu);
     return (
         screen
     )
 }
-
-
-
-/*
-    //const [optionScreen, setOptionScreen] = useState(false);
-    //const [leaderboardScreen, setLeaderboardScreen] = useState(false);
-   //onClick={()=> {setOptionScreen((prevState)=>!prevState); setStartScreen((prev)=>!prev)}
-   //onClick={() => {setLeaderboardScreen((prev)=>!prev)(}}
-  const particlesInit = useCallback( async (engine) => {
-        await loadFull(engine);
-    }, []); 
-
-    //let content = (optionScreen && <MapMenu setGameImage={setGameImage} />) || (leaderboardScreen && <Leaderboard />) || <HomeMenu setStartScreen={setStartScreen} setLeaderboardScreen={setLeaderboardScreen} setOptionScreen={setOptionScreen}/>;
-    //let screen = ApplyParticle(HomeMenu, {setStartScreen, setOptionScreen, setLeaderboardScreen});
-         <StartPage  exit={{opacity:0}} transition={{duration:0.5}}  >
-            <Particle id="tsparticles" options={particlesOptions} init={particlesInit} />
-            {content}
-        </StartPage> 
-*/
