a184e5ec4a95c8cc8e23fa0cd5a451fcfa01c70f
MingMing <greeteaz567@gmail.com>
2022-08-23 18:38:03 -0500

    Removed un-needed styled-component

--- src/components/ApplyParticle.js
+++ src/components/ApplyParticle.js
@@ -1,21 +1,7 @@
-import styled from "styled-components";
-import { motion } from "framer-motion";
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
 import { useCallback } from "react";
 import particlesOptions from "../particles.json";
-
-const Page = styled(motion.main)`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap:25px;
-    min-width: 100vw;
-    width:100%;
-    margin:0; padding:0;
-    color:white;
-`;
 
 export default function ApplyParticle(props) {
     const particlesInit = useCallback(async (engine) => {
