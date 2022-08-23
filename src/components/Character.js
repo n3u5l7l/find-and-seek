39054c942b0f42691a3edeab645427444071fbed
MingMing <greeteaz567@gmail.com>
2022-08-22 20:11:17 -0500

    Styled found characters

--- src/components/Character.js
+++ src/components/Character.js
@@ -3,23 +3,53 @@ import styled from "styled-components";
 const CustomSection = styled.section`
     display: none;
     align-items: center;
+    gap:15px;
 
     & > .character{
         display:flex;
         flex-direction: column;
         align-items: center;
+        position: relative;
+    }
+    & > .character.found{
+        opacity:0.5;
+    }
+    & > .character.found::before{
+        content: "";
+        width: 100%;
+        position: absolute;
+        top: 50%;
+        left:0;
+        height: 3px;
+        background: gray;
+        transform:translateY(-50%);
     }
     & > .character > img{
         width: 70px;
         height:auto;
     }
+
+    & > .character > div{
+        position: relative;
+    }
+
+    & > .character.found > div::before{
+        content: "";
+        width: 100%;
+        position: absolute;
+        top: 50%;
+        left:0;
+        height: 3px;
+        background: gray;
+        transform:translateY(-50%);
+    }
 `
-export default function Character ({characters}){
+export default function Character ({characters, deleteChar}){
 
     return (
         <CustomSection className="character-dropdown-list">
             {characters.map((char, index) => (
-                <div className="character" key={index}>
+                <div className={deleteChar===char.name?"character found": "character"} key={index}>
                     <img src={char.image}  alt={char.name}></img>
                     <div>{char.name}</div>
                 </div>
