e1a0700342c942fb650e3f353b09d493a16fd14b
MingMing <greeteaz567@gmail.com>
2022-08-27 20:40:16 -0500

    Removed unnecessary imports

--- src/components/GameScreen.js
+++ src/components/GameScreen.js
@@ -2,10 +2,6 @@ import Header from "./Header";
 import Game from "./Game";
 import React from "react";
 import { useState } from "react";
-import CyberPunkSrc from "../assets/egor-klyuchnyk-full-x-season-web.jpg";
-import AshKetchum from "../assets/ash-ketch-1.png";
-import Tom from "../assets/tom-tom-and-jerry-png-418409.png"
-import { Navigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import { MapInfo } from "./Menu";
 import Character from "./Character";
